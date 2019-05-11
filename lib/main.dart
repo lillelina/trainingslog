d3a8a08d70f9c355e2a71c51e05b2a0a4574e0ea
Elina Eickstädt <elina.eickstaedt@gmail.com>
2019-05-11 09:48:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  //This widget is root of the application
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter login UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(title: 'Flutter Login'),
+    );
+  }
+}
+
